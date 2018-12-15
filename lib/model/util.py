a9b87cbd6c79630b9d01db5218f9a2ffe6a169c7
Achyudh Ram <achyudhk@gmail.com>
2018-12-15 01:52:00 -0500

    Pickle embeddings on disk for faster load times

--- lib/model/util.py
+++ lib/model/util.py
@@ -1,6 +1,6 @@
 import codecs
 import os
-
+import dill
 import numpy as np
 from keras.callbacks import LearningRateScheduler
 from tqdm import tqdm
@@ -19,22 +19,26 @@ def lr_scheduler(initial_lr, decay_factor):
 
 
 def embedding_matrix(model_path, vocab, embed_dim=300):
-    embed_index = dict()
-    file_sizes = {'wiki.en.vec': 2519428, 'wiki.de.vec': 2275261, 'wiki.fr.vec': 1152450, 'wiki.vi.vec': 292169}
-    if model_path not in file_sizes:
-        Exception("Unsupported embedding file")
-    print("Loading embedding matrix from:", os.path.basename(model_path))
+    if os.path.isfile(model_path + '.cache'):
+        with open(model_path + '.cache', 'rb') as pkl_file:
+            return dill.load(pkl_file)
+    else:
+        embed_index = dict()
+        file_sizes = {'wiki.en.vec': 2519428, 'wiki.de.vec': 2275261, 'wiki.fr.vec': 1152450, 'wiki.vi.vec': 292169}
+        print("Loading embedding matrix from:", os.path.basename(model_path))
 
-    with codecs.open(model_path, encoding='utf-8') as embedding_file:
-        for line in tqdm(embedding_file, total=file_sizes[os.path.basename(model_path)]):
-            values = line.rstrip().rsplit(' ')
-            word = values[0]
-            coefs = np.asarray(values[1:], dtype='float32')
-            embed_index[word] = coefs
+        with codecs.open(model_path, encoding='utf-8') as embedding_file:
+            for line in tqdm(embedding_file, total=file_sizes[os.path.basename(model_path)]):
+                values = line.rstrip().rsplit(' ')
+                word = values[0]
+                coefs = np.asarray(values[1:], dtype='float32')
+                embed_index[word] = coefs
 
-    embed_matrix = np.zeros((len(vocab), embed_dim))
-    for word, i in vocab.items():
-        embedding_vector = embed_index.get(word)
-        if (embedding_vector is not None) and len(embedding_vector) > 0:
-            embed_matrix[i] = embedding_vector
-    return embed_matrix
+        embed_matrix = np.zeros((len(vocab), embed_dim))
+        for word, i in vocab.items():
+            embedding_vector = embed_index.get(word)
+            if (embedding_vector is not None) and len(embedding_vector) > 0:
+                embed_matrix[i] = embedding_vector
+        with open(model_path + '.cache', 'wb') as pkl_file:
+            dill.dump(embed_matrix, pkl_file)
+        return embed_matrix
