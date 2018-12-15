439fcf94199331827e454addaf2083e0f5d31689
Achyudh Ram <achyudhk@gmail.com>
2018-12-15 00:47:50 -0500

    Add en_vi support to driver method

--- lib/model/__main__.py
+++ lib/model/__main__.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 from lib.data import fetch
 from lib.data.generator import WMTSequence
 from lib.model.util import embedding_matrix
+from lib.model import metrics
 from lib.model.args import get_args
 from lib.model.seq2seq import Seq2Seq, TinySeq2Seq
 
@@ -16,21 +17,50 @@ if __name__ == '__main__':
     if args.dataset == 'en_de':
         encoder_train_input, decoder_train_input, decoder_train_target, source_vocab, target_vocab = \
             fetch.en_de(args.dataset_path)
+        encoder_dev_input, decoder_dev_input, decoder_dev_target, source_vocab, target_vocab = \
+            fetch.en_de(args.dataset_path, source_vocab, target_vocab, splits='dev')
         encoder_test_input, decoder_test_input, decoder_test_target, source_vocab, target_vocab = \
             fetch.en_de(args.dataset_path, source_vocab, target_vocab, one_hot=True, splits='test')
-        source_embedding_map = embedding_matrix('data/embeddings/wiki.en.vec', source_vocab)
-        target_embedding_map = embedding_matrix('data/embeddings/wiki.de.vec', target_vocab)
+        source_embedding_map = embedding_matrix(os.path.join(args.embedding_path, 'wiki.en.vec'), source_vocab)
+        target_embedding_map = embedding_matrix(os.path.join(args.embedding_path, 'wiki.de.vec'), target_vocab)
+
     elif args.dataset == 'de_en':
         encoder_train_input, decoder_train_input, decoder_train_target, source_vocab, target_vocab = \
             fetch.en_de(args.dataset_path, reverse=True)
+        encoder_dev_input, decoder_dev_input, decoder_dev_target, source_vocab, target_vocab = \
+            fetch.en_de(args.dataset_path, source_vocab, target_vocab, reverse=True, splits='dev')
         encoder_test_input, decoder_test_input, decoder_test_target, source_vocab, target_vocab = \
             fetch.en_de(args.dataset_path, source_vocab, target_vocab, one_hot=True, reverse=True, splits='test')
-        source_embedding_map = embedding_matrix('data/embeddings/wiki.de.vec', source_vocab)
-        target_embedding_map = embedding_matrix('data/embeddings/wiki.en.vec', target_vocab)
+        source_embedding_map = embedding_matrix(os.path.join(args.embedding_path, 'wiki.de.vec'), source_vocab)
+        target_embedding_map = embedding_matrix(os.path.join(args.embedding_path, 'wiki.en.vec'), target_vocab)
+
+    if args.dataset == 'en_vi':
+        encoder_train_input, decoder_train_input, decoder_train_target, source_vocab, target_vocab = \
+            fetch.en_vi(args.dataset_path)
+        encoder_dev_input, decoder_dev_input, decoder_dev_target, source_vocab, target_vocab = \
+            fetch.en_vi(args.dataset_path, source_vocab, target_vocab, splits='dev')
+        encoder_test_input, decoder_test_input, decoder_test_target, source_vocab, target_vocab = \
+            fetch.en_vi(args.dataset_path, source_vocab, target_vocab, one_hot=True, splits='test')
+        source_embedding_map = embedding_matrix(os.path.join(args.embedding_path, 'wiki.en.vec'), source_vocab)
+        target_embedding_map = embedding_matrix(os.path.join(args.embedding_path, 'wiki.vi.vec'), target_vocab)
+
+    elif args.dataset == 'vi_en':
+        encoder_train_input, decoder_train_input, decoder_train_target, source_vocab, target_vocab = \
+            fetch.en_vi(args.dataset_path, reverse=True)
+        encoder_dev_input, decoder_dev_input, decoder_dev_target, source_vocab, target_vocab = \
+            fetch.en_vi(args.dataset_path, source_vocab, target_vocab, reverse=True, splits='dev')
+        encoder_test_input, decoder_test_input, decoder_test_target, source_vocab, target_vocab = \
+            fetch.en_vi(args.dataset_path, source_vocab, target_vocab, one_hot=True, reverse=True, splits='test')
+        source_embedding_map = embedding_matrix(os.path.join(args.embedding_path, 'wiki.vi.vec'), source_vocab)
+        target_embedding_map = embedding_matrix(os.path.join(args.embedding_path, 'wiki.en.vec'), target_vocab)
+
     else:
         raise Exception("Unsupported dataset")
 
     model = None
+    metrics.DATASET = args.dataset
+    metrics.TARGET_VOCAB = target_vocab
+
     model_config = deepcopy(args)
     source_vocab_size = len(source_vocab)
     target_vocab_size = len(target_vocab)
@@ -42,14 +72,12 @@ if __name__ == '__main__':
     model_config.target_embedding_map = target_embedding_map
 
     training_generator = WMTSequence(encoder_train_input, decoder_train_input, decoder_train_target, model_config)
-    # TODO: Create a separate validation split for the validation generator
-    # validation_generator = WMTSequence(encoder_test_input, decoder_test_input, decoder_test_target, model_config)
+    validation_generator = WMTSequence(encoder_dev_input, decoder_dev_input, decoder_dev_target, model_config)
 
     if args.cpu:
         model = TinySeq2Seq(args)
     else:
         model = Seq2Seq(model_config)
 
-    # model.train(encoder_train_input, decoder_train_input, decoder_train_target)
-    model.train_generator(generator=training_generator)
+    model.train_generator(training_generator, validation_generator)
     model.evaluate(encoder_test_input, decoder_test_input, decoder_test_target)
