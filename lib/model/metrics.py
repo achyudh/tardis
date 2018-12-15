1eca9844a36e01c0c132a67a1dfd844bb5306520
Achyudh Ram <achyudhk@gmail.com>
2018-12-15 00:48:18 -0500

    Squash bugs in model/metrics

--- lib/model/metrics.py
+++ lib/model/metrics.py
@@ -3,16 +3,20 @@ from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu
 
 from lib.data.util import reverse_indexing
 
+# Global variables
+DATASET = None
+TARGET_VOCAB = None
 
-def bleu_score(reference, candidate, target_vocab):
+
+def bleu_score(reference, candidate):
     # TODO: Find a workaround for Keras metric API limitation
-    reference = reverse_indexing(reference, target_vocab)
-    candidate = reverse_indexing(candidate, target_vocab)
+    reference = reverse_indexing(reference, TARGET_VOCAB, ravel=True)
+    candidate = reverse_indexing(candidate, TARGET_VOCAB)
     return corpus_bleu(reference, candidate, smoothing_function=SmoothingFunction().method4)
 
 
-def multi_bleu_score(candidate, target_vocab, dataset):
-    lang_pair = '-'.join(dataset.split('_'))
+def multi_bleu_score(candidate, target_vocab):
+    lang_pair = '-'.join(DATASET.split('_'))
     candidate = reverse_indexing(candidate, target_vocab)
     _, *refs = sacrebleu.download_test_set('wmt14', lang_pair)
     bleu = sacrebleu.corpus_bleu(candidate, refs)
