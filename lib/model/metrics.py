9d7145ece3b02c942c92e6e865d49035e63f0d5f
Achyudh Ram <achyudhk@gmail.com>
2018-12-16 18:41:17 -0500

    Fix bug in BLEU score metric

--- lib/model/metrics.py
+++ lib/model/metrics.py
@@ -12,11 +12,12 @@ TARGET_VOCAB = None
 
 def bleu_score(reference, candidate, log_outputs=True):
     # TODO: Find a workaround for Keras metric API limitation
-    reference = reverse_indexing(reference, TARGET_VOCAB, ravel=True)
+    reference = [[x] for x in reference]
+    # reference = reverse_indexing(reference, TARGET_VOCAB, ravel=True)
     candidate = reverse_indexing(candidate, TARGET_VOCAB)
     if log_outputs:
-        with open('%s_output.json' % DATASET) as json_file:
-            json.dump(zip(reference, candidate), json_file)
+        with open('%s_output.json' % DATASET, 'w') as json_file:
+            json.dump(list(zip(reference, candidate)), json_file)
     return corpus_bleu(reference, candidate, smoothing_function=SmoothingFunction().method4)
 
 
