48ad3c3cb3838416a9f59f7a0d8feac14e0752d8
Achyudh Ram <achyudhk@gmail.com>
2018-12-16 18:40:52 -0500

    Use Adam instead of SGD for optimization

--- lib/model/seq2seq.py
+++ lib/model/seq2seq.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from keras.initializers import RandomUniform
 from keras.layers import Input, LSTM, Embedding, Dense
 from keras.models import Model
-from keras.optimizers import SGD
+from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 
 from lib.model.metrics import bleu_score
@@ -45,7 +45,7 @@ class Seq2Seq:
         # Input: Source and target sentence, Output: Predicted translation
         self.model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
-        optimizer = SGD(lr=config.lr, momentum=0.0, clipnorm=25.)
+        optimizer = Adam(lr=config.lr, clipnorm=25.)
         self.model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['acc'])
         print(self.model.summary())
 
@@ -54,8 +54,8 @@ class Seq2Seq:
             'full_%s_ep{epoch:02d}_nl%d_sv%d_tv%d.hdf5' % (self.config.dataset, self.config.num_layers,
                                                            self.config.source_vocab_size, self.config.target_vocab_size)
         callbacks = [lr_scheduler(initial_lr=self.config.lr, decay_factor=self.config.decay),
-                     ModelCheckpoint(os.path.join('data', 'checkpoints', checkpoint_filename),
-                                     monitor='val_loss', verbose=0, save_best_only=False,
+                     ModelCheckpoint(os.path.join(os.getcwd(), 'data', 'checkpoints', checkpoint_filename),
+                                     monitor='val_loss', verbose=1, save_best_only=False,
                                      save_weights_only=True, mode='auto', period=1)]
         self.model.fit([encoder_train_input, decoder_train_input], decoder_train_target,
                        batch_size=self.config.batch_size,
@@ -73,7 +73,7 @@ class Seq2Seq:
 
     def evaluate(self, encoder_predict_input, decoder_predict_input, test_target, log_outputs=True):
         y_pred = self.model.predict([encoder_predict_input, decoder_predict_input])
-        print("BLEU Score:", bleu_score(y_pred, test_target))
+        print("BLEU Score:", bleu_score(test_target, y_pred))
         # An error in the sacrebleu library prevents multi_bleu_score from working on WMT '14 EN-DE test split
         # print("Multi-BLEU Score", multi_bleu_score(y_pred, self.config.target_vocab, self.config.dataset))
 
