2a4ac3f23992cd338d65075c9b21f17de82d2619
Achyudh Ram <achyudhk@gmail.com>
2018-12-16 21:14:17 -0500

    Trim output based on EOS before calculating BLEU

--- lib/model/metrics.py
+++ lib/model/metrics.py
@@ -3,7 +3,7 @@ import json
 import sacrebleu
 from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu
 
-from lib.data.util import reverse_indexing
+from lib.data.util import reverse_index, trim_sentences
 
 # Global variables
 DATASET = None
@@ -12,18 +12,18 @@ TARGET_VOCAB = None
 
 def bleu_score(reference, candidate, log_outputs=True):
     # TODO: Find a workaround for Keras metric API limitation
-    reference = [[x] for x in reference]
     # reference = reverse_indexing(reference, TARGET_VOCAB, ravel=True)
-    candidate = reverse_indexing(candidate, TARGET_VOCAB)
+    candidate = trim_sentences(reverse_index(candidate, TARGET_VOCAB))
     if log_outputs:
         with open('%s_output.json' % DATASET, 'w') as json_file:
             json.dump(list(zip(reference, candidate)), json_file)
+    reference = [[x] for x in reference]
     return corpus_bleu(reference, candidate, smoothing_function=SmoothingFunction().method4)
 
 
 def multi_bleu_score(candidate, target_vocab):
     lang_pair = '-'.join(DATASET.split('_'))
-    candidate = reverse_indexing(candidate, target_vocab)
+    candidate = reverse_index(candidate, target_vocab)
     _, *refs = sacrebleu.download_test_set('wmt14', lang_pair)
     bleu = sacrebleu.corpus_bleu(candidate, refs)
     return bleu.score
